435bfa0a7d0f8ca9aeef9f8fb2364eebc90957a4
Dan Luo <luo_dana@yahoo.com>
2023-01-30 08:43:41 +1000

    01

--- sign.py
+++ sign.py
@@ -7,12 +7,15 @@ from hashlib import sha256
 def sign(m):
 	#generate public key
 	#Your code here
-	public_key = None
+	n = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141
+	d = random.randrange(1,n)
+	public_key = fastecdsa.keys.get_public_key(d,fastecdsa.curve.Curve('SECP256K1'))
+	sig = fastecdsa.ecdsa.sign(M, d, fastecdsa.curve.Curve('SECP256K1'), hashfunc=SHA256, False)
 
 	#generate signature
 	#Your code here
-	r = 0
-	s = 0
+	r = sig[0]
+	s = sig[1]
 
 	assert isinstance( public_key, point.Point )
 	assert isinstance( r, int )
