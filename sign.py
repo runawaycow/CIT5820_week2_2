bc2d53e14f1a402f2aaae96b4794b6f690110721
Dan Luo <luo_dana@yahoo.com>
2023-01-30 08:45:16 +1000

    01

--- sign.py
+++ sign.py
@@ -4,6 +4,8 @@ from fastecdsa.keys import export_key, gen_keypair
 from fastecdsa import curve, ecdsa, keys, point
 from hashlib import sha256
 
+import random
+
 def sign(m):
 	#generate public key
 	#Your code here
